3ac0b46bba73c3aa215c3a758bf1a84141600272
prateek singh <singh_prateek@outlook.com>
2021-11-28 22:16:10 +0530

    heap sort added.

---
+++ skiena-book/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "ps";
+Console.WriteLine($"Hello, World! {name}");
+
+HeapSort m = new(HeapType.MinHeap);
+const int length = 7;
+int[] src = new int[length] { 48, 24, 12, 10, 23, 21, 101 };
+
+Console.WriteLine(string.Join(", ", m.createHeap(src, length)[1..]));
+
+Console.WriteLine("\nsorted");
+
+Console.WriteLine(string.Join(", ", m.Sort(src)));
