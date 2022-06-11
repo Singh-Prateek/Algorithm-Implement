5a0c8b59559bd048b66b5c28fbfd7565ea6c81c7
Prateek Singh <singh.prateek@outlook.in>
2022-06-11 22:57:11 +0530

    fix directed edge

--- skiena-book/Console.Application/Graph.cs
+++ skiena-book/Console.Application/Graph.cs
@@ -4,10 +4,10 @@ public class Graph
 {
     private readonly List<EdgeNode>[] _edgeNodes;
     private IReadOnlyList<List<EdgeNode>>? edgeNodes;
-    private readonly List<int> _degree = new();
+    private readonly int[] _degree = Array.Empty<int>();
     private int _edgeCount = 0;
     public IReadOnlyList<List<EdgeNode>> EdgeNodes => this.GetEdgeNode();
-    public IReadOnlyList<int> Degree => _degree.AsReadOnly();
+    public IReadOnlyList<int> Degree => _degree.ToList().AsReadOnly();
     public int VerticesCount { get; init; }
     public int EdgesCount => _edgeCount;
     public bool Directed { get; init; }
@@ -16,23 +16,28 @@ public class Graph
     {
         VerticesCount = verticesCount;
         Directed = directed;
-        _edgeNodes = new List<EdgeNode>[verticesCount+1];
+        _degree = new int[verticesCount + 1];
+        _edgeNodes = new List<EdgeNode>[verticesCount + 1];
     }
 
 
-    public void AddEdge(int x, int y, int weight)
+    public void AddEdge(int x, int y, int weight, bool directed = false)
     {
-       
         if (_edgeNodes[x] == null)
         {
             _edgeNodes[x] = new List<EdgeNode>();
         }
 
-        _edgeNodes[x].Add(new EdgeNode() { Y = y, Weight = weight });
-        
-        if (this.Directed)
+        if (!_edgeNodes[x].Any(n => n.Y == y))
         {
-            AddEdge(y, x, weight);
+            _edgeNodes[x].Add(new EdgeNode() { Y = y, Weight = weight });
+        }
+
+        _degree[x]++;
+
+        if (!directed)
+        {
+            AddEdge(y, x, weight, true);
         }
         else
         {
