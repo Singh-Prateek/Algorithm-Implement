0065c3d040fd230738f7b9c960753d704422a75c
Prateek Singh <singh.prateek@outlook.in>
2022-06-26 17:39:43 +0530

    added backtracking

---
+++ skiena-book/Console.Application/Backtracking.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace skiena.book.combinationsearch;
+
+public class Backtracking
+{
+    public void AlgoRun()
+    {
+        Backtrack(new int[] { 0, 0, 0, 0 }, 0, 3);
+
+    }
+
+    private void Backtrack(int[] a, int k, int n)
+    {
+        int nc;
+        int[] c;
+
+        if (IsASolution(a, k, n))
+        {
+            ProcessSolution(a, k, n);
+        }
+        else
+        {
+            k += 1;
+            c = ConstructCandidates(a, k);
+            nc = c.Length;
+
+            for (int i = 0; i < nc; i++)
+            {
+                a[k] = c[i];
+                MakeMove(a, k);
+                Backtrack(a, k, n);
+                UnMakeMove(a, k);
+
+            }
+        }
+
+    }
+
+    private void UnMakeMove(int[] a, int k)
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void MakeMove(int[] a, int k)
+    {
+        //throw new NotImplementedException();
+    }
+
+    private int[] ConstructCandidates(int[] a, int k)
+    {
+        return new int[]
+        {
+            1,
+            0
+        };
+
+    }
+
+    private void ProcessSolution(int[] a, int k, int input)
+    {
+        Console.Write("{");
+        for (int i = 1; i <= k; i++)
+        {
+            if (a[i] == 1)
+            {
+                Console.Write(i);
+            }
+
+        }
+
+        Console.Write("} ");
+    }
+
+    private bool IsASolution(int[] a, int k, int n)
+    {
+        return k == n;
+    }
+}
+
