e958186b5e1a97df72f9c61e60331512016ed27c
Prateek Singh <singh.prateek@outlook.in>
2022-09-15 19:27:14 +0530

    added valid brack check

--- leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
+++ leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
@@ -1,27 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using LeetCodeProblems;
-using static LeetCodeProblems.DecimalNumsToRomanNums;
+using static LeetCodeProblems.ValidParentheses;
 
 Console.WriteLine("Hello, World!");
 
-//var result = TwoSums.TwoSumMakingNumber(new int[] { -3, 2, 3, 4, 6, 8, 9 }, 0);
-
-//var result = ThreeSumProblem.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
-//var result = ThreeSumProblem.ThreeSum(new int[] { -1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4 });
-// var result = MergedSortedArray.FindMedianSortedArrays(
-//     new int[] { 1, 1, 1 },
-//     new int[] { 1, 1, 1 }
-//     );
-
-
-// Console.WriteLine(result);
-//Console.WriteLine(String.Join(" | ", result.Select(s => "[" + string.Join(",", s) + "]")));
-//var result = FindDuplicate(new int[] { 2, 5, 9, 6, 9, 3, 8, 9, 7, 1 });
-System.Console.WriteLine(IntToRoman(3));
-System.Console.WriteLine(IntToRoman(40));
-System.Console.WriteLine(IntToRoman(123));
-System.Console.WriteLine(IntToRoman(23));
-System.Console.WriteLine(IntToRoman(1200));
-System.Console.WriteLine(IntToRoman(3900));
+Console.WriteLine(IsValid("]"));
+Console.WriteLine(IsValid("()"));
+Console.WriteLine(IsValid("([{}])"));
+Console.WriteLine(IsValid("[}"));
 
---
+++ leetcode/LeetCodeProblems/LeetCodeProblems/ValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace LeetCodeProblems;
+
+internal class ValidParentheses
+{
+    internal static bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+
+        Dictionary<char, int> start = new() { { '(', 1 }, { '{', 2 }, { '[', 3 } };
+        Dictionary<char, int> end = new() { { ')', 1 }, { '}', 2 }, { ']', 3 } };
+
+        int peekCheck = 0;
+
+        foreach (char c in s)
+        {
+
+            if (start.ContainsKey(c))
+            {
+                stack.Push(c);
+            }
+
+            if (end.ContainsKey(c))
+            {
+                if (stack.Any())
+                {
+                    start.TryGetValue(stack.Peek(), out peekCheck);
+                }
+                else
+                {
+                    peekCheck = 0;
+                }
+
+                if (peekCheck == end[c])
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
+
