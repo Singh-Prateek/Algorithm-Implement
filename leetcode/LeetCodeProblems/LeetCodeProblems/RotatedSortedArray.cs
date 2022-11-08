49a6c341662819db621fe8de1e59b809a8ced721
Prateek Singh <singh.prateek@outlook.in>
2022-11-08 18:50:17 +0530

    added search in rotated array

--- leetcode/LeetCodeProblems/LeetCodeProblems/RotatedSortedArray.cs
+++ leetcode/LeetCodeProblems/LeetCodeProblems/RotatedSortedArray.cs
@@ -7,23 +7,27 @@ public class RotatedSortedArray
     {
         RotatedSortedArray obj = new RotatedSortedArray();
 
-        RunWithMessages(obj, "5,1,2,3,4");
+        RunWithMessages(obj, "5,1,3", 0);
+        RunWithMessages(obj, "4,5,6,7,0,1,2", 3);
+        RunWithMessages(obj, "5,1,2,3,4", 4);
         RunWithMessages(obj, "3,4,5,1,2");
         RunWithMessages(obj, "1,2,3,4,5");
-        RunWithMessages(obj, "2,3,4,5,1");
+        RunWithMessages(obj, "2,3,4,5,1", 1);
+        RunWithMessages(obj, "1", 1);
         RunWithMessages(obj, "6,1,2,3,4,5");
-        RunWithMessages(obj, "1,2,4,5,6,7,0");
+        RunWithMessages(obj, "1,2,4,5,6,7");
         RunWithMessages(obj, "11,12,43");
         RunWithMessages(obj, "11,12");
         RunWithMessages(obj, "222222,111111");
     }
 
-    private static void RunWithMessages(RotatedSortedArray runner, string s)
+    private static void RunWithMessages(RotatedSortedArray runner, string s, int target = 1)
     {
         int index = 0;
-        Console.WriteLine("{0} - source : {1}\tmin :{2}", ++index, s, FindMinInArray(GetIntArray(s)));
-        Console.WriteLine("{0} - source : {1}\tmin :{2}", ++index, s, runner.FindMinRecursion(GetIntArray(s)));
-        Console.WriteLine("{0} - source : {1}\tmax :{2}", ++index, s, FindMaxInArray(GetIntArray(s)));
+        Console.WriteLine("{0} - source :{1}\tmin :{2}", ++index, s, FindMinInArray(GetIntArray(s)));
+        Console.WriteLine("{0} - source :{1}\tmin :{2}", ++index, s, runner.FindMinRecursion(GetIntArray(s)));
+        Console.WriteLine("{0} - source :{1}\tmax :{2}", ++index, s, FindMaxInArray(GetIntArray(s)));
+        Console.WriteLine("{0} - source :{1}\ttarget :{2}\tidx :{3}", ++index, s, target, FindInArray(GetIntArray(s), target));
         Console.WriteLine("");
     }
 
@@ -112,6 +116,50 @@ public class RotatedSortedArray
 
         //Console.WriteLine("max idx:{0}", (l + nums.Length - 1) % nums.Length);
         return nums[l];
+    }
+
+    private static int FindInArray(int[] nums, int target)
+    {
+        int l = 0;
+        int m = 0;
+        int r = nums.Length - 1;
+
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+
+            if (nums[m] == target) return m;
+
+            //left sorted
+            if (nums[l] <= nums[m])
+            {
+                if (target < nums[l] || target > nums[m])
+                {
+                    l = m + 1;
+                }
+                else
+                {
+                    r = m - 1;
+                }
+
+            }
+            else
+            {
+                //right sorted
+                if (target < nums[m] || target > nums[r])
+                {
+                    r = m - 1;
+                }
+                else
+                {
+                    l = m + 1;
+                }
+
+            }
+
+        }
+
+        return -1;
     }
 
 }
