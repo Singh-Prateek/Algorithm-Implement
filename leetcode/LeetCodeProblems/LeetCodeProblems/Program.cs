960d2b95e188378bfbaf48a1d891ac5462248256
Prateek Singh <singh.prateek@outlook.in>
2022-11-09 13:34:45 +0530

    update project to dotnet 7

--- leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
+++ leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using LeetCodeProblems;
-using static LeetCodeProblems.ValidParentheses;
 
 Console.WriteLine("Hello, World!");
 
