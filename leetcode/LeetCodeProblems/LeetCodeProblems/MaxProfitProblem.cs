bc857a2efaf9218353514a2b5e4391d03d142dc8
Prateek Singh <singh.prateek@outlook.in>
2022-11-12 16:36:59 +0530

    fix print formatting

---
+++ leetcode/LeetCodeProblems/LeetCodeProblems/MaxProfitProblem.cs
@@ -0,0 +1,60 @@
+﻿namespace LeetCodeProblems;
+
+public class MaxProfitProblem
+{
+    public static void Run(int[] prices)
+    {
+        var pricesStr = string.Join(',',prices);
+        Console.WriteLine("1-source:{0} \tmax: {1}",pricesStr, MaxProfit(prices));
+        Console.WriteLine("2-source:{0} \tmax: {1}",pricesStr ,MaxProfitTwo(prices));
+    }
+
+    static int MaxProfit(int[] prices)
+    {
+        int mP = 0;
+        int rP = 0;
+
+        int mbIdx = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+
+            rP = prices[i] - prices[mbIdx];
+
+            if (prices[i] < prices[mbIdx])
+            {
+                mbIdx = i;
+            }
+
+            mP = Math.Max(mP, rP);
+
+        }
+
+        return mP;
+
+    }
+
+    static int MaxProfitTwo(int[] prices)
+    {
+        int mP = 0;
+        int rP = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+
+            rP += prices[i] - prices[i - 1];
+
+
+            mP = Math.Max(rP, mP);
+            //Console.WriteLine("running: {0} \t max: {1};\t i:{2}", rP, mP, i);
+
+            if (rP < 0)
+            {
+                rP = 0;
+            }
+        }
+
+        return mP;
+    }
+}
+
--- leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
+++ leetcode/LeetCodeProblems/LeetCodeProblems/Program.cs
@@ -1,16 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics;
 using LeetCodeProblems;
 
 Console.WriteLine("Hello, World!");
 
-MostWaterProblem.Run();
-//RotatedSortedArray.Run();
-//Console.ReadKey();
-
-
-
-
+MaxProfitProblem.Run("7,1,5,2,3,6,4".Split(',').Select(int.Parse).ToArray());
+MaxProfitProblem.Run(new int[] { 2, 1, 2, 1, 0, 1, 2 });
 
 
 
